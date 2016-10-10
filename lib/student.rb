e751706818ffa866091a9f9629e3542b40ef4eab
Ben <henriquez.ben@gmail.com>
2016-10-10 00:11:48 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,15 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
+
+  def self.find_by(hash)
+    key = hash.keys.join
+    value = hash.values.join
+    sql = "SELECT * FROM #{self.table_name} WHERE #{key} = '#{value}' "
+    DB[:conn].execute(sql)
+end
 
 end
